722893aac71075079018eb920ef1083c6a7cd12d
Aligar Syahan Putra <aligar.gligar@gmail.com>
2020-06-25 06:18:33 +0700

    asd

--- src/page/Home.js
+++ src/page/Home.js
@@ -6,24 +6,13 @@ import SuhuKeluarModal from '../components/SuhuKeluarModal';
 import {Button} from '../components/Buttons';
 
 const Home = () => {
-    const [modalSuhuOpen, setModalSuhuOpen] = useState(false)
 
     return (
         <Div>
-            <center>
-                <Button onClick={() => setModalSuhuOpen(true)} set='primary' type="submit"> 
-                    Update Suhu Keluar 
-                </Button>
-            </center>
-
             <SuhuKeluarModal 
                 isOpen={modalSuhuOpen}
                 setOpen={x => setModalSuhuOpen(x)}
             />
-
-            <br/>
-
-            <MainCalendar />
         </Div>
     )
 }
